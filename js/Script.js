348f25303476ace3438e8dd8a8e7b61335242a7a
DiluniMalsha <51900011+DiluniMalsha@users.noreply.github.com>
2019-08-18 19:54:36 +0530

    1.0.0

---
+++ js/Script.js
@@ -0,0 +1,167 @@
+$("#navHome").click(
+    function () {
+        $("#dashboardPanel").css("display","inline");
+        $("#customerPanel").css("display","none");
+        $("#customerViewPanel").css("display","none");
+        $("#customerUpdatePanel").css("display","none");
+        $("#itemPanel").css("display","none");
+        $("#itemViewPanel").css("display","none");
+        $("#itemUpdatePanel").css("display","none");
+        $("#orderPanel").css("display","none");
+        $("#orderViewPanel").css("display","none");
+    }
+);
+
+$("#navCustomer").click(
+    function () {
+        $("#dashboardPanel").css("display","none");
+        $("#customerPanel").css("display","none");
+        $("#customerViewPanel").css("display","inline");
+        $("#customerUpdatePanel").css("display","none");
+        $("#itemPanel").css("display","none");
+        $("#itemViewPanel").css("display","none");
+        $("#itemUpdatePanel").css("display","none");
+        $("#orderPanel").css("display","none");
+        $("#orderViewPanel").css("display","none");
+    }
+);
+
+$("#navItem").click(
+    function () {
+        $("#dashboardPanel").css("display","none");
+        $("#customerPanel").css("display","none");
+        $("#customerViewPanel").css("display","none");
+        $("#customerUpdatePanel").css("display","none");
+        $("#itemPanel").css("display","none");
+        $("#itemViewPanel").css("display","inline");
+        $("#itemUpdatePanel").css("display","none");
+        $("#orderPanel").css("display","none");
+        $("#orderViewPanel").css("display","none");
+    }
+);
+
+$("#navOrder").click(
+    function () {
+        $("#dashboardPanel").css("display","none");
+        $("#customerPanel").css("display","none");
+        $("#customerViewPanel").css("display","none");
+        $("#customerUpdatePanel").css("display","none");
+        $("#itemPanel").css("display","none");
+        $("#itemViewPanel").css("display","none");
+        $("#itemUpdatePanel").css("display","none");
+        $("#orderPanel").css("display","none");
+        $("#orderViewPanel").css("display","inline");
+    }
+);
+
+$("#addNewCustomer").click(
+    function () {
+        $("#dashboardPanel").css("display","none");
+        $("#customerPanel").css("display","inline");
+        $("#customerViewPanel").css("display","none");
+        $("#customerUpdatePanel").css("display","none");
+        $("#itemPanel").css("display","none");
+        $("#itemViewPanel").css("display","none");
+        $("#itemUpdatePanel").css("display","none");
+        $("#orderPanel").css("display","none");
+        $("#orderViewPanel").css("display","none");
+    }
+);
+
+$("#addNewItem").click(
+    function () {
+        $("#dashboardPanel").css("display","none");
+        $("#customerPanel").css("display","none");
+        $("#customerViewPanel").css("display","none");
+        $("#customerUpdatePanel").css("display","none");
+        $("#itemPanel").css("display","inline");
+        $("#itemViewPanel").css("display","none");
+        $("#itemUpdatePanel").css("display","none");
+        $("#orderPanel").css("display","none");
+        $("#orderViewPanel").css("display","none");
+    }
+);
+
+$("#addNewOrder").click(
+    function () {
+        $("#dashboardPanel").css("display","none");
+        $("#customerPanel").css("display","none");
+        $("#customerViewPanel").css("display","none");
+        $("#customerUpdatePanel").css("display","none");
+        $("#itemPanel").css("display","none");
+        $("#itemViewPanel").css("display","none");
+        $("#itemUpdatePanel").css("display","none");
+        $("#orderPanel").css("display","inline");
+        $("#orderViewPanel").css("display","none");
+    }
+);
+
+$("#viewCustomers").click(
+    function () {
+        $("#dashboardPanel").css("display","none");
+        $("#customerPanel").css("display","none");
+        $("#customerViewPanel").css("display","inline");
+        $("#customerUpdatePanel").css("display","none");
+        $("#itemPanel").css("display","none");
+        $("#itemViewPanel").css("display","none");
+        $("#itemUpdatePanel").css("display","none");
+        $("#orderPanel").css("display","none");
+        $("#orderViewPanel").css("display","none");
+    }
+);
+
+$("#viewCustomersFromUpdate").click(
+    function () {
+        $("#dashboardPanel").css("display","none");
+        $("#customerPanel").css("display","none");
+        $("#customerViewPanel").css("display","inline");
+        $("#customerUpdatePanel").css("display","none");
+        $("#itemPanel").css("display","none");
+        $("#itemViewPanel").css("display","none");
+        $("#itemUpdatePanel").css("display","none");
+        $("#orderPanel").css("display","none");
+        $("#orderViewPanel").css("display","none");
+    }
+);
+
+$("#viewItems").click(
+    function () {
+        $("#dashboardPanel").css("display","none");
+        $("#customerPanel").css("display","none");
+        $("#customerViewPanel").css("display","none");
+        $("#customerUpdatePanel").css("display","none");
+        $("#itemPanel").css("display","none");
+        $("#itemViewPanel").css("display","inline");
+        $("#itemUpdatePanel").css("display","none");
+        $("#orderPanel").css("display","none");
+        $("#orderViewPanel").css("display","none");
+    }
+);
+
+$("#viewItemsFromUpdate").click(
+    function () {
+        $("#dashboardPanel").css("display","none");
+        $("#customerPanel").css("display","none");
+        $("#customerViewPanel").css("display","none");
+        $("#customerUpdatePanel").css("display","none");
+        $("#itemPanel").css("display","none");
+        $("#itemViewPanel").css("display","inline");
+        $("#itemUpdatePanel").css("display","none");
+        $("#orderPanel").css("display","none");
+        $("#orderViewPanel").css("display","none");
+    }
+);
+
+$("#viewOrders").click(
+    function () {
+        $("#dashboardPanel").css("display","none");
+        $("#customerPanel").css("display","none");
+        $("#customerViewPanel").css("display","none");
+        $("#customerUpdatePanel").css("display","none");
+        $("#itemPanel").css("display","none");
+        $("#itemViewPanel").css("display","none");
+        $("#itemUpdatePanel").css("display","none");
+        $("#orderPanel").css("display","none");
+        $("#orderViewPanel").css("display","inline");
+    }
+);
